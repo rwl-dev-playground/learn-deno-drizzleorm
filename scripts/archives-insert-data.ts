749d147f55a5ada2c20b6133f030fb129123a3c0
WhyK <windchimeyk@gmail.com>
2023-08-06 17:13:14 +0900

    chore: add archive script

---
+++ scripts/archives-insert-data.ts
@@ -0,0 +1,33 @@
+import { connect } from "planetscale";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { movieTable } from "../db/schema/movie.ts";
+import { generateInsertSql, writeInsertSqlFile } from "./lib.ts";
+import "std/dotenv/load.ts";
+
+const connection = connect({
+  host: Deno.env.get("PS_HOST"),
+  username: Deno.env.get("DEVELOP")
+    ? Deno.env.get("PS_DEV_USERNAME")
+    : Deno.env.get("PS_USERNAME"),
+  password: Deno.env.get("DEVELOP")
+    ? Deno.env.get("PS_DEV_PASSWORD")
+    : Deno.env.get("PS_PASSWORD"),
+});
+const db = drizzle(connection);
+
+const movieResult = await db.select({
+  title: movieTable.title,
+  is_dubbed: movieTable.is_dubbed,
+  is_domestic: movieTable.is_domestic,
+  is_live_action: movieTable.is_live_action,
+  theater_id: movieTable.theater_id,
+  view_date: movieTable.view_date,
+  view_start_time: movieTable.view_start_time,
+  view_end_time: movieTable.view_end_time,
+  accompanier: movieTable.accompanier,
+  rating: movieTable.rating,
+  comment: movieTable.comment,
+}).from(movieTable);
+const movieInsertSql = generateInsertSql("tbl_movieinfo", movieResult);
+
+await writeInsertSqlFile("movie", movieInsertSql);
